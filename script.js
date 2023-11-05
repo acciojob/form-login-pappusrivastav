f55fe5d31912a902c1c15b923c1b4904152f2813
pappusrivastav <144468141+pappusrivastav@users.noreply.github.com>
2023-11-05 19:20:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-// function getFormvalue() {
-//     //Write your code here
-
-// }
 function getFormvalue() {
-  const form = document.getElementById("form1");
-  const firstName = form.fname.value;
-  const lastName = form.lname.value;
-  alert(`First name: ${firstName}\nLast name: ${lastName}`);
-  return false; // to prevent the form from submitting
+    // Get the form elements by their name attributes
+    const firstName = document.forms["form1"]["fname"].value;
+    const lastName = document.forms["form1"]["lname"].value;
+    
+    // Format the alert message to match the expected format
+    alert(firstName + ' ' + lastName);
+
+    // Prevent the form from submitting and refreshing the page
+    return false;
 }
+
